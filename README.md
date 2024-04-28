cbf99ea5dece385a3cdcca51bf002ebebe63aa72
Tania <128644557+taniafollonier@users.noreply.github.com>
2024-04-28 20:35:27 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,70 @@
+<p align="center">
+  <img src="./Images/Steam Games Banner.gif"
+  <b style="font-size: 24px;"> PROYECTO INDIVIDUAL 1: MLOPS</b> :rocket:
+</p>
+
+<p align="center">
+ <b style="font-size: 24px;"> PLATAFORMA STEAM GAMES</b> :joystick:
+</p>
+
+<p align="center">
+  :small_orange_diamond: <i><u>INTRODUCCIÓN:</u></i> :small_orange_diamond:
+</p>
+
+En este proyecto se va a trabajar sobre la plataforam de juegos Steam, desarrollando un rol de Data Engineer para lograr tener un MVP (Minimum Viable Product). En base a 3 datasets iniciales, se va a desarrollar el correspondiente proceso de ETL (Extracción, Transformación y Carga) y de EDA (Análisis Exploratorio de Datos). Además, se va a realizar un  análisis de sentimiento con NLP, y un Modelo de aprendizaje automático, con un sistema de recomendación.
+
+<p align="center">
+  :small_orange_diamond: <i><u>ARCHIVOS INICIALES:</u></i> :small_orange_diamond:
+</p>
+
+Se tienen 3 datasets:
+• australian_user_reviews.json: Conjunto de datos con id de usuarios y sus comentrios de los juegos, su recomendación o no, así como también la url del perfil de usuario y el id del juego.
+
+• australian_users_items.json: Conjunto de datos con información de los juegos, y el tiempo acumulado de juego por cada usuario.
+
+• output_steam_games.json: Conjunto de datos con títulos, géneros, id de los juegos, sus precios y características.
+
+<p align="center">
+  :small_orange_diamond: <i><u>PROCESO DE ETL, EDA y FEATURE ENGINEERING:</u></i> :small_orange_diamond:
+</p>
+
+Como se mencionó anteriormente, como primera medida se llevó a cabo un proceso de ETL (Extracción, Transformación y Carga), analizando el tipo de dato de cada columna de los distintos datasets, transformándolos cuando fuera necesario, eliminando duplicados, eliminando columnas con valores nulos, desañidando 2 columnas que estaban - justamente - añidadas. También se procedió a eliminar las columnas que no iban a ser de utilidad para el posterior análisis y funcionamiento de api.
+Para la realización de la consigna de realizar un un análisis de sentimiento a los comentarios de los usuarios, se introdujo una nueva columna llamada 'sentiment_analysis', la cual sustituye a la columna que originalmente contenía los comentarios de los usuarios. Esta columna clasifica los sentimientos de los comentarios según la siguiente escala:  0 si el sentimiento es negativo, 1 si es neutral o si no hay un comentario asociado,  2 si el sentimiento es positivo. 
+Dado que se pedía que se aplicara un análisis de sentimiento con NLP, se utilizó la biblioteca TextBlob, que clasifica la polaridad del texto como positiva, negativa o neutra. 
+Se guardaron los datasets limpios en archivos parquet. 
+Luego se procedió a la realización del EDA (Análisis Exploratorio de Datos), para identificar los datos necesarios para la posterior realización del modelo de recomendación. Se usaron las librerías Matplotlib y Seaborn para la visualización.
+Todo lo antedicho se puede ver en los archivos:  
+[ETL_Steam_Games](./Jupyter/ETL_Steam_Games.ipynb)  
+[ETL_user_items](./Jupyter/ETL_user_items.ipynb)  
+[ETL_users_reviews](./Jupyter/ETL_users_reviews.ipynb)  
+[Feature_Engineering_EDA](./Jupyter/Feature_Engineering_EDA.ipynb)  
+
+
+<p align="center">
+  :small_orange_diamond: <i><u>API:</u></i> :small_orange_diamond:
+</p>
+
+El desarrollo de la API se realizó usando el framework FastAPI, generando las 5 funciones propuestas para las consultas:
+• _PlayTimeGenre:_ Debe devolver año con mas horas jugadas para dicho género.
+
+• _UserForGenre:_ Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+
+• _UsersRecommend:_ Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado.
+
+• _UsersNotRecommend:_ Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado.
+
+• _sentiment_analysis( año : int ):_ Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.
+
+Posteriormente, se realizó el Modelo de Recomendación Automático, utilizando el sistema de recomendación item-item. Para su realización se utilizó la similitud del coseno, que determina cuán similares son dos conjuntos de datos o elementos, y se calcula utilizando el coseno del ángulo entre los vectores que representan esos datos o elementos.
+• _recomendacion_juego( id de producto ):_ Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
+
+Lo antedicho se puede observar en el archivo [main](./main.py)  
+[FastAPI](http://127.0.0.1:8000/)
+
+<p align="center">
+  :small_orange_diamond: <i><u>DEPLOYMENT:</u></i> :small_orange_diamond:
+</p>
+
+Luego de verificar que la API funciona a nivel local, se procedió a usar Render para que la misma pueda ser consumida desde la web. Dado que el servicio gratuito de Render consta de poca memoria, se optó por un muestreo porcentual de los Dataframes pertinentes.
+
+#Lo antedicho se puede observar en el siguiente link: [Render](https://pi1-mlops-steam-games.onrender.com)
---
+++ main.py
@@ -0,0 +1,367 @@
+from fastapi import Depends, FastAPI, HTTPException, Path
+from fastapi.responses import HTMLResponse
+import pandas as pd
+from typing import List, Dict
+import os
+import pyarrow.parquet as pq
+import asyncio
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import gzip
+
+
+parquet_file_path1 = "Jupyter/df_PlayTimeGenre_gzip.parquet"
+parquet_file_path2 = "Jupyter/df_UserForGenre_gzip.parquet"
+parquet_file_path3 = "Jupyter/df_UsersRecommend_gzip.parquet"
+parquet_file_path4 = "Jupyter/df_sentiment_analysis_gzip.parquet"
+parquet_file_path5 = "Jupyter/df_RecomendacionJuego_gzip.parquet"
+
+
+app = FastAPI(title= 'Proyecto Integrador 1',
+              description= 'Machine Learning Operations (MLOps), por Camila Fernández Llaneza',
+              version= '1.0.1', debug=True)
+
+
+
+
+@app.get('/PlayTimeGenre/{genero}')
+async def PlayTimeGenre(genero: str):
+    '''
+    Datos:
+    - genero (str): Género para el cual se busca el año con más horas jugadas.
+
+    Funcionalidad:
+    - Devuelve el año con más horas jugadas para el género especificado.
+
+    Return:
+    - Dict: {"Año de lanzamiento con más horas jugadas para Género X": int}
+    '''
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file1 = pq.ParquetFile(parquet_file_path1)
+        total_rows1 = parquet_file1.metadata.num_rows
+        sample_rows1 = int(total_rows1 * (sample_percent / 100.0))
+        df_PlayTimeGenre_muestra = parquet_file1.read_row_groups(row_groups=[0]).to_pandas().head(sample_rows1//80)
+        genero_filtrado = df_PlayTimeGenre_muestra[df_PlayTimeGenre_muestra['genres'].apply(lambda x: genero in x)]
+
+        if genero_filtrado.empty:
+            raise HTTPException(status_code=404, detail=f"No hay datos para el género {genero}")
+
+        genero_filtrado['playtime_forever'] = genero_filtrado['playtime_forever'] / 60
+
+        max_hours_year = genero_filtrado.groupby('release_date')['playtime_forever'].sum().idxmax()
+
+        return {"Año de lanzamiento con más horas jugadas para el Género " + genero: int(max_hours_year)}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get('/UserForGenre/{genero}')
+async def UserForGenre(genero:str):
+    '''
+    Datos:
+    - genero (str): Género para el cual se busca el usuario con más horas jugadas y la acumulación de horas por año.
+
+    Funcionalidad:
+    - Devuelve el usuario con más horas jugadas y una lista de la acumulación de horas jugadas por año para el género especificado.
+
+    Return:
+    - Dict: {"Usuario con más horas jugadas para Género X": List, "Horas jugadas": List}
+    '''
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file2 = pq.ParquetFile(parquet_file_path2)
+        total_rows2 = parquet_file2.metadata.num_rows
+        sample_rows2 = int(total_rows2 * (sample_percent / 100.0))
+        df_UserForGenre_muestra = parquet_file2.read_row_groups(row_groups=[0]).to_pandas().head(sample_rows2//80)
+          
+        condition = df_UserForGenre_muestra['genres'].apply(lambda x: genero in x)
+        juegos_genero = df_UserForGenre_muestra[condition]
+
+       
+        juegos_genero['playtime_forever'] = juegos_genero['playtime_forever'] / 60
+        juegos_genero['release_date'] = pd.to_numeric(juegos_genero['release_date'], errors='coerce')
+        juegos_genero = juegos_genero[juegos_genero['release_date'] >= 100]
+        juegos_genero['Año'] = juegos_genero['release_date']
+
+        horas_por_usuario = juegos_genero.groupby(['user_id', 'Año'])['playtime_forever'].sum().reset_index()
+        if not horas_por_usuario.empty:
+            usuario_max_horas = horas_por_usuario.groupby('user_id')['playtime_forever'].sum().idxmax()
+            usuario_max_horas = horas_por_usuario[horas_por_usuario['user_id'] == usuario_max_horas]
+        else:
+            usuario_max_horas = None
+
+        acumulacion_horas = horas_por_usuario.groupby(['Año'])['playtime_forever'].sum().reset_index()
+        acumulacion_horas = acumulacion_horas.rename(columns={'Año': 'Año', 'playtime_forever': 'Horas'})
+
+        resultado = {
+            "Usuario con más horas jugadas para " + genero: {"user_id": usuario_max_horas.iloc[0]['user_id'], "Año": int(usuario_max_horas.iloc[0]['Año']), "playtime_forever": usuario_max_horas.iloc[0]['playtime_forever']},
+            "Horas jugadas": [{"Año": int(row['Año']), "Horas": row['Horas']} for _, row in acumulacion_horas.iterrows()]
+        }
+
+        return resultado
+        
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Error al cargar los archivos de datos")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get('/UsersRecommend/{anio}')
+async def UsersRecommend(anio: int):
+    '''
+    Datos:
+    - anio (int): Año para el cual se busca el top 3 de juegos más recomendados.
+
+    Funcionalidad:
+    - Devuelve el top 3 de juegos más recomendados por usuarios para el año dado.
+
+    Return:
+    - List: [{"Puesto 1": str}, {"Puesto 2": str}, {"Puesto 3": str}]
+    '''
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file3 = pq.ParquetFile(parquet_file_path3)
+        total_rows3 = parquet_file3.metadata.num_rows
+        sample_rows3 = int(total_rows3 * (sample_percent / 100.0))
+        df_UsersRecommend_muestra = parquet_file3.read_row_groups(row_groups=[0]).to_pandas().head(sample_rows3)
+
+        filtered_df = df_UsersRecommend_muestra[
+        (df_UsersRecommend_muestra["reviews_posted"] == anio) &
+        (df_UsersRecommend_muestra["reviews_recommend"] == True) &
+        (df_UsersRecommend_muestra["sentiment_analysis"]>=1)
+        ]
+        recommend_counts = filtered_df.groupby("title")["title"].count().reset_index(name="count").sort_values(by="count", ascending=False).head(3)
+        top_3_dict = {f"Puesto {i+1}": juego for i, juego in enumerate(recommend_counts['title'])}
+        return top_3_dict
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al obtener los juegos mas recomendados.")
+
+@app.get('/UsersNotRecommend/{anio}')
+async def UsersNotRecommend(anio: int):
+    '''
+    Datos:
+    - anio (int): Año para el cual se busca el top 3 de juegos menos recomendados.
+
+    Funcionalidad:
+    - Devuelve el top 3 de juegos menos recomendados por usuarios para el año dado.
+
+    Return:
+    - List: [{"Puesto 1": str}, {"Puesto 2": str}, {"Puesto 3": str}]
+    '''
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file4 = pq.ParquetFile(parquet_file_path3)
+        total_rows3 = parquet_file4.metadata.num_rows
+        sample_rows3 = int(total_rows3 * (sample_percent / 100.0))
+        df_UsersRecommend_muestra = parquet_file4.read_row_groups(row_groups=[0]).to_pandas().head(sample_rows3)
+
+        filtered_df = df_UsersRecommend_muestra[
+        (df_UsersRecommend_muestra["reviews_posted"] == anio) &
+        (df_UsersRecommend_muestra["reviews_recommend"] == False) &
+        (df_UsersRecommend_muestra["sentiment_analysis"]==0)
+        ]
+        not_recommend_counts = filtered_df.groupby("title")["title"].count().reset_index(name="count").sort_values(by="count", ascending=False).head(3)
+        top_3_dict = {f"Puesto {i+1}": juego for i, juego in enumerate(not_recommend_counts['title'])}
+        return top_3_dict
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al obtener los juegos menos recomendados.")
+    
+@app.get('/sentiment_analysis/{anio}')
+async def sentiment_analysis(anio: int):
+
+    '''
+    Según el año de lanzamiento, se devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento.
+
+    Args:
+        año (int): Año para el cual se busca el análisis de sentimiento.
+
+    Returns:
+        dict: Diccionario con la cantidad de reseñas por sentimiento.
+    '''
+  
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file5 = pq.ParquetFile(parquet_file_path4)
+        total_rows5 = parquet_file5.metadata.num_rows
+        sample_rows5 = int(total_rows5 * (sample_percent / 100.0))
+        df_sentiment_analysis_muestra = parquet_file5.read_row_groups(row_groups=[0]).to_pandas().head(sample_rows5)
+        
+        filtered_df = df_sentiment_analysis_muestra[df_sentiment_analysis_muestra["release_date"] == anio]
+
+        
+        sentiment_counts = filtered_df["sentiment_analysis"].value_counts()
+
+        
+        sentiment_mapping = {2: "Positive", 1: "Neutral", 0: "Negative"}
+        sentiment_counts_mapped = {sentiment_mapping[key]: value for key, value in sentiment_counts.items()}
+
+        return sentiment_counts_mapped
+    except pd.errors.EmptyDataError:
+        raise HTTPException(status_code=404, detail=f"No hay datos para el año {anio}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+@app.get('/Recomendacion_Juego/{id_producto}')
+async def recomendacion_juego(id_producto: int = Path(..., description="ID del juego para obtener recomendaciones")):
+    '''
+    Endpoint para obtener una lista de juegos recomendados similares a un juego dado.
+
+    Parámetros:
+    - id_juego (int): ID del juego para el cual se desean obtener recomendaciones.
+
+    Respuestas:
+    - 200 OK: Retorna una lista con 5 juegos recomendados similares al juego ingresado.
+    - 404 Not Found: Si no se encuentra el juego con el ID especificado.
+    - 500 Internal Server Error: En caso de cualquier otro error, proporciona detalles de la excepción.
+
+    Ejemplo de Uso:
+    - /RecomendarJuego/123
+
+    Ejemplo de Respuesta Exitosa:
+    [
+        {"id": 456, "nombre": "Juego A"},
+        {"id": 789, "nombre": "Juego B"},
+        {"id": 101, "nombre": "Juego C"},
+        {"id": 202, "nombre": "Juego D"},
+        {"id": 303, "nombre": "Juego E"}
+    ]
+    '''
+    try:
+        sample_percent = 5
+
+        # Lee una muestra del archivo Parquet con pyarrow
+        parquet_file6 = pq.ParquetFile(parquet_file_path5)
+        total_rows6 = parquet_file6.metadata.num_rows
+        sample_rows6 = int(total_rows6 * (sample_percent / 100.0))
+        df_RecomendacionJuego_muestra = parquet_file6.read_row_groups(row_groups=[0]).to_pandas()
+
+        porcentaje_muestra = 50
+        total_registros = len(df_RecomendacionJuego_muestra)
+        num_registros = int(total_registros * (porcentaje_muestra / 100.0))
+        df_subset = df_RecomendacionJuego_muestra.sample(n=num_registros, random_state=42).reset_index(drop=True)
+        
+        num_recommendations = 5
+
+        juego_seleccionado = df_subset[df_subset['item_id'] == id_producto]
+
+        if juego_seleccionado.empty:
+            raise HTTPException(status_code=404, detail=f"No se encontró el juego con ID {id_producto}")
+
+        title_game_and_genres = ' '.join(juego_seleccionado['title'].fillna('').astype(str) + ' ' + juego_seleccionado['genres'].fillna('').astype(str))
+        tfidf_vectorizer = TfidfVectorizer()
+        tfidf_matrix = tfidf_vectorizer.fit_transform(df_subset['title'].fillna('').astype(str) + ' ' + df_subset['genres'].fillna('').astype(str))
+
+        juego_tfidf = tfidf_vectorizer.transform([title_game_and_genres])
+        similarity_scores = cosine_similarity(juego_tfidf, tfidf_matrix)
+
+        if similarity_scores is not None:
+            similar_games_indices = similarity_scores[0].argsort()[::-1]
+
+            
+            recommended_games = df_subset.loc[similar_games_indices[1:]]
+            recommended_games = recommended_games[~recommended_games['item_id'].isin([id_producto])].drop_duplicates(subset='title')
+
+            recommendations_list = recommended_games.head(num_recommendations)['title'].tolist()
+
+            if len(recommendations_list) < num_recommendations:
+                message = f"Se encontraron {len(recommendations_list)} recomendaciones para este ID."
+                recommendations_list += [None] * (num_recommendations - len(recommendations_list))
+            else:
+                message = None
+
+            return {"recomendaciones": recommendations_list, "message": message}
+        else:
+            return {"message": "No se encontraron juegos similares."}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}") from e
+
+
+
+
+
+# Inicio
+    
+@app.get("/", response_class=HTMLResponse, tags=["Home"])
+async def presentacion():
+    return '''
+        <html>
+            <head>
+                <title>API Steam</title>
+                <style>
+                    body {
+                        color: black; 
+                        background-color: white; 
+                        font-family: Arial, sans-serif;
+                        padding: 20px;
+                    }
+                    h1 {
+                        color: #333;
+                        text-align: center;
+                    }
+                    p {
+                        color: #666;
+                        text-align: center;
+                        font-size: 18px;
+                        margin-top: 20px;
+                    }
+                    footer {
+                        text-align: center;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Proyecto Individual N° 1: MLOps Steam</h1>
+                <p>Esta es una API para consultas de la plataforma Steam.</p>
+                <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual para ingresar.</p>
+                
+            </body>
+        </html>
+    '''
+
+
+# Funciones
+
+@app.get(path='/PlayTimeGenre/{genero}', tags=["Funciones Generales"])
+def play_time_genre(genero: str = Path(..., description="Devuelve el año con más horas jugadas para el género especificado (Ingresar la primer letra en Mayúscula)")):
+    return PlayTimeGenre(genero)
+
+@app.get(path='/UserForGenre/{genero}', tags=["Funciones Generales"])
+def user_for_genre(genero: str = Path(..., description="Devuelve el usuario que acumula más horas jugadas para el género especificado (Ingresar la primer letra en Mayúscula)")):
+    return UserForGenre(genero)
+
+
+@app.get("/UsersRecommend/{anio}", tags=["Funciones Generales"])
+def users_recommend(anio: int = Path(..., description="Devuelve el top 3 de juegos más recomendados para el año especificado")):
+        try:
+            result = UsersRecommend(anio)
+            return result
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+@app.get(path='/UsersNotRecommend/{anio}', tags=["Funciones Generales"])
+def users_not_recommend(anio: int = Path(..., description="Devuelve el top 3 de juegos menos recomendados para el año especificado")):
+    return UsersNotRecommend(anio)
+
+@app.get(path='/sentiment_analysis/{anio}', tags=["Funciones Generales"])
+def sentiment_analysis(anio: int = Path(..., description="Devuelve una lista con la cantidad de registros de reseñas de usuarios que se encuentran categorizados con un análisis de sentimiento en el año especificado")):
+    return sentiment_analysis(anio)
+
+@app.get(path='/Recomendacion_Juego/{id_producto}', tags=["Sistema de Recomendación: Item-Item"])
+async def recomendacion_juego(id_producto: int = Path(..., description= "Devuelve una lista con 5 juegos recomendados similares al ingresado")):
+    return recomendacion_juego(id_producto)
